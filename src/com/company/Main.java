d05ed8af979b2af96767d2111697a3314ff9f28f
Marcin <hulkcinek@gmail.com>
2020-05-10 15:25:18 +0200

    zamiana16na10

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,16 +9,10 @@ public class Main {
     4. Zamiana liczby binarnej na dziesietna (rekurencyjnie)
      */
 
-    public static String zamiana10na2iteracyjnie(int liczba) {
-        String wynik = "";
-        while (liczba>0){
-            wynik = liczba%2 + wynik; // dopisanie z przodu zmiennej wynik
-            liczba = liczba/2; // liczba /= 2;
-        }
-        return wynik;
-    }
     // zasada dzialania ^ - XOR (ksor)
     /*
+    Do zrobienia na potem: operacje arytmetyczne na liczbach binarnych
+
     OR: (daje true gdy co najmniej jedno jest prawdziwe)
     true || true => true
     true || false => true
@@ -38,6 +32,16 @@ public class Main {
     --
     01
     */
+
+    public static String zamiana10na2iteracyjnie(int liczba) {
+        String wynik = "";
+        while (liczba>0){
+            wynik = liczba%2 + wynik; // dopisanie z przodu zmiennej wynik
+            liczba = liczba/2; // liczba /= 2;
+        }
+        return wynik;
+    }
+
     public static int zamiana2na10iteracyjnie(String binarna) {
         int wynik = 0;
         for (int i = 0; i < binarna.length(); i++) {
@@ -58,23 +62,78 @@ public class Main {
 
     public static int zamiana2na10rekurencyjnie(String binarna, int ktora){
         if (ktora == binarna.length()-1) {
-            return binarna.charAt(ktora) - '0';
+            return binarna.charAt(ktora) - '0'; // nie mnozymy przez 2^0 bo to i tak jest 1
         }
-        return (zamiana2na10rekurencyjnie(binarna.charAt(ktora) - '0') < (binarna.length()-ktora-1)) +
-                zamiana2na10rekurencyjnie(binarna, ktora+1);
+
+        int cyfra = (binarna.charAt(ktora) - '0');
+        int wynik = (int)(cyfra*Math.pow(2,binarna.length()-1-ktora));
+
+        return zamiana2na10rekurencyjnie(binarna, ktora+1)+wynik;
+    }
+
+    public static String zamiana10na8(int liczba) {
+        int wynik;
+        if(liczba > 0) {
+            wynik = liczba % 8;
+            return zamiana10na8(liczba / 8)  + wynik;
+        }
+        return "";
+    }
+
+    public static int zamiana8na10(String osemkowa, int ktora) {
+        if (ktora == osemkowa.length()-1) {
+            return osemkowa.charAt(ktora) - '0'; // nie mnozymy przez 2^0 bo to i tak jest 1
+        }
+
+        int cyfra = (osemkowa.charAt(ktora) - '0');
+        int wynik = (int)(cyfra*Math.pow(8,osemkowa.length()-1-ktora));
+
+        return zamiana8na10(osemkowa, ktora+1)+wynik;
+    }
+
+    public static String zamiana10na16(int liczba) {
+        String wynik = "";
+        if(liczba > 0) {
+            int resztaZDzielenia = liczba % 16;
+            if (resztaZDzielenia>9){
+                wynik += (char)('A' + resztaZDzielenia-10); // mozna tez jako 'A' + resztaZDzielenia%10
+            }else {
+                wynik += resztaZDzielenia;
+            }
+            return zamiana10na16(liczba / 16)  + wynik;
+        }
+        return "";
     }
 
 
+    // do dokonczenia bazujac na zamianie z 8 na 10
+    public static int zamiana16na10(String szesnastkowa, int ktora) {
+        if (ktora == szesnastkowa.length()-1) {
+            return szesnastkowa.charAt(ktora) - '0'; // nie mnozymy przez 2^0 bo to i tak jest 1
+        }
+        int cyfra;
+        if(szesnastkowa.charAt(ktora) >= 'A'){
+            cyfra = szesnastkowa.charAt(ktora) - 'A' + 10;
+        }else {
+            cyfra = (szesnastkowa.charAt(ktora) - '0');
+        }
+        int wynik = (int)(cyfra*Math.pow(16,szesnastkowa.length()-1-ktora));
+
+        return zamiana16na10(szesnastkowa, ktora+1)+wynik;
+    }
+
     /*
-    Zastanowic sie nad zadaniami z rekurencja^
-    (odpowiedzi sa baardzo podobne do wersji iteracyjnej)
-    +
-    https://pl.spoj.com/problems/SKARBFI.pdf
+    Jak napisac funkcje ktora zrobi konwersje z dowolnego systemu na dowolny w formie:
+    public static String konwertuj(int systemWejsciowy, int systemWyjsciowy, String liczbaDoZamiany);
      */
     public static void main(String[] args) {
         System.out.println(zamiana10na2iteracyjnie(10));
         System.out.println(zamiana2na10iteracyjnie("1101011"));
         System.out.println(zamiana10na2rekurencyjnie(10));
         System.out.println(zamiana2na10rekurencyjnie("1101011", 0));
+        System.out.println(zamiana10na8(37));
+        System.out.println(zamiana8na10("45",0));
+        System.out.println(zamiana10na16(934));
+        System.out.println(zamiana16na10("3A6", 0));
     }
 }
