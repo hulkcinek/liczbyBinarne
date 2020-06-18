000aaa3e98d0e2afb9db659a93dd5c08248b369c
Marcin <hulkcinek@gmail.com>
2020-06-18 12:42:43 +0200

    nieudana proba zamainy dowolnej

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     /*
@@ -122,10 +124,45 @@ public class Main {
         return zamiana16na10(szesnastkowa, ktora+1)+wynik;
     }
 
+    // mozna zrobic na zasadzie zamiany z input na 10, a potem z 10 na output
+    public static String zamianaDowolna(int system, int systemWyjsciowy, String input){
+        int dziesiatkowa = zamianaDowolnaNa10(input, system, 0);
+        return zamiana10NaDowolna(dziesiatkowa);
+    }
+
+    private static int zamianaDowolnaNa10(String input, int system, int ktora) {
+        if (ktora == input.length()-1) {
+            return input.charAt(ktora) - '0'; // nie mnozymy przez 2^0 bo to i tak jest 1
+        }
+        int cyfra;
+        if(input.charAt(ktora) >= 'A'){
+            cyfra = input.charAt(ktora) - 'A' + 10;
+        }else {
+            cyfra = (input.charAt(ktora) - '0');
+        }
+        int wynik = (int)(cyfra*Math.pow(system,input.length()-1-ktora));
+
+        return zamiana16na10(input, ktora+1)+wynik;
+    }
+
+    private static String zamiana10NaDowolna(int dziesiatkowa) {
+        String wynik = "";
+        if(dziesiatkowa > 0) {
+            int resztaZDzielenia = dziesiatkowa % 16;
+            if (resztaZDzielenia>9){
+                wynik += (char)('A' + resztaZDzielenia-10); // mozna tez jako 'A' + resztaZDzielenia%10
+            }else {
+                wynik += resztaZDzielenia;
+            }
+            return zamiana10na16(dziesiatkowa / 16)  + wynik;
+        }
+        return "";
+    }
+
     /*
-    Jak napisac funkcje ktora zrobi konwersje z dowolnego systemu na dowolny w formie:
-    public static String konwertuj(int systemWejsciowy, int systemWyjsciowy, String liczbaDoZamiany);
-     */
+        Jak napisac funkcje ktora zrobi konwersje z dowolnego systemu na dowolny w formie:
+        public static String konwertuj(int systemWejsciowy, int systemWyjsciowy, String liczbaDoZamiany);
+         */
     public static void main(String[] args) {
         System.out.println(zamiana10na2iteracyjnie(10));
         System.out.println(zamiana2na10iteracyjnie("1101011"));
@@ -135,5 +172,8 @@ public class Main {
         System.out.println(zamiana8na10("45",0));
         System.out.println(zamiana10na16(934));
         System.out.println(zamiana16na10("3A6", 0));
+        System.out.println();
+        System.out.println(zamianaDowolna(16,8,"12E"));
+        Scanner s = new Scanner(System.in);
     }
 }
